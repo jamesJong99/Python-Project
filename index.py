f97fdbea872d116ac5599cd61d225945a222493b
jamesJong99 <jamesjong99@gmail.com>
2024-05-02 09:47:55 -0700

    update test

--- index.py
+++ index.py
@@ -3,4 +3,5 @@ fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = [x for x in fruits if "a" in x]
 fruits.sort(reverse = True)
 print ('sort of furits:', fruits) # sort list
+newlist.sort()
 print(newlist)
